be8992a6e48ba28ee82ee5681bfdeba481b6dbb7
elzup <guild0105@gmail.com>
2017-11-04 02:50:29 +0900

    Test room logic

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ function gio(port, ns) {
 	const store = {}
 	nsp.on('connection', socket => {
 		console.log('new connection: ' + socket.id)
-		socket.broadcast.emit('msg', { event: 'connected', id: socket.id })
 		store[socket.id] = { id: socket.id }
 
 		socket.on('join', packet => {
 			socket.join(packet.room)
 			const { id, room, profile } = packet
 			store[id] = { room, profile }
+			socket
+				.to(room)
+				.broadcast.emit('msg', { event: 'connected', id: socket.id })
 		})
 
 		socket.on('msg', packet => {
--- test/test.js
+++ test/test.js
@@ -7,30 +7,59 @@ const connect = require('socket.io-client')
 
 const gio = require(path.resolve(__dirname, '../src/'))
 const ns = '/test'
-const { io, nsp } = gio(8080, ns)
+let io, nsp
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec))
 
+let i = 0
+beforeEach(() => {
+	i++
+	const p = gio(8080, ns + i)
+	io = p.io
+	nsp = p.nsp
+})
+
+afterEach(() => {
+	io.close()
+})
+
 describe('server', () => {
-	before(() => {})
-
-	after(() => {
-		io.close()
-	})
-
 	it('should get connected', done => {
-		const first = connect(`http://localhost:8080${ns}`)
+		const first = connect(`http://localhost:8080${ns + i}`)
 		first.on('connect', () => {
 			first.emit('join', { room: 'a-room', profile: { m: 'hello' } })
 			first.on('msg', data => {
+				console.log(data)
 				assert.equal(data.event, 'connected')
 				done()
 			})
 		})
 		sleep(100).then(() => {
-			const second = connect(`http://localhost:8080${ns}`)
+			const second = connect(`http://localhost:8080${ns + i}`)
 			second.on('connect', () => {
 				second.emit('join', { room: 'a-room', profile: { m: 'yo' } })
+			})
+		})
+	})
+
+	it('should get message', done => {
+		console.log('2')
+		const first = connect(`http://localhost:8080${ns + i}`)
+		first.on('connect', () => {
+			first.emit('join', { room: 'a-room', profile: { m: 'hello' } })
+			sleep(100).then(() => {
+				first.on('msg', data => {
+					console.log(data)
+					assert.equal(data.hoge, 'fuga')
+					done()
+				})
+			})
+		})
+
+		const second = connect(`http://localhost:8080${ns + i}`)
+		second.on('connect', () => {
+			second.emit('join', { room: 'a-room', profile: { m: 'yo' } })
+			sleep(200).then(() => {
 				second.emit('msg', { hoge: 'fuga' })
 			})
 		})
