14d4d7bf9fb0d38ebec41f4565f20aac9f2016de
elzup <guild0105@gmail.com>
2017-11-04 17:37:46 +0900

    Update test safety

--- test/test.js
+++ test/test.js
@@ -41,7 +41,7 @@ describe('server', () => {
 	it('should get message', done => {
 		const first = connect(`http://localhost:8080${ns + i}`)
 		first.emit('join', { room: 'a-room', profile: { m: 'hello' } })
-		sleep(100).then(() => {
+		sleep(200).then(() => {
 			first.on('msg', data => {
 				assert.equal(data.hoge, 'fuga')
 				done()
@@ -50,7 +50,7 @@ describe('server', () => {
 
 		const second = connect(`http://localhost:8080${ns + i}`)
 		second.emit('join', { room: 'a-room', profile: { m: 'yo' } })
-		sleep(200).then(() => {
+		sleep(400).then(() => {
 			second.emit('msg', { hoge: 'fuga' })
 		})
 	})
